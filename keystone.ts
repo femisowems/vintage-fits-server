53c6d7d8972f1a89ad93ed61ef516333e77a162b
Femi Sowemimo <femi.sowems@gmail.com>
2023-12-10 16:32:50 -0500

    feat: fixed server error, added product and pics

---
+++ keystone.ts
@@ -0,0 +1,58 @@
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { KeystoneConfig } from '@keystone-next/types';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-vintage-fits';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // TODO: change this for roles
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name email',
+    }),
+    // TODO: Add session values here
+  })
+);
